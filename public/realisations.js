73cfa0408a62b9bee4a6e0cc1152abe739f97d44
Lefebvre genius <lefebvregenius@gmail.com>
2026-06-10 14:45:17 +0100

    modification realisations

--- public/realisations.js
+++ public/realisations.js
@@ -28,3 +28,13 @@ card.style.transform =
 });
 
 });
+
+
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  navbar.classList.toggle(
+    "scrolled",
+    window.scrollY > 50
+  );
+});
